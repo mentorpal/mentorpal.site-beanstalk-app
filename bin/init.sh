3fa8f584312872dedaf6c8da301538dd163c0530
larry kirschner <larrykirschner@gmail.com>
2021-05-03 13:14:48 -0700

    improves README and version scripts (#12)
    * improves README and version scripts

--- bin/init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# sets the upstream remote and installs git lfs
-
-UPSTREAM=https://github.com/mentorpal/aws-beanstalk-app.git
-git remote show upstream > /dev/null 2>&1 && echo "" || git remote add upstream $UPSTREAM 
-git lfs install
